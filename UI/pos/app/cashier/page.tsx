b96ebd50d14c9e5965b7e16b7a989ffef9f6f904
jesse-kariuki <kariuki.jesse@strathmore.edu>
2026-03-03 10:44:33 +0300

    modified each product to have a delete button

--- UI/pos/app/cashier/page.tsx
+++ UI/pos/app/cashier/page.tsx
@@ -706,7 +706,6 @@ export default function CashierDashboard() {
                     <div>
                       <label className="block text-sm font-semibold text-gray-300 mb-2">Amount Received</label>
                       <div className="relative">
-                        <FaCalculator className="absolute left-4 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" />
                         <input
                           type="number"
                           value={cashGiven}
